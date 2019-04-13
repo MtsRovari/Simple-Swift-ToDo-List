dda881ccacaf6bf6fd8b37b22896d85146afbdd6
Mateus Rovari <mateusrovari@Mateuss-MacBook-Pro.local>
2019-04-13 20:24:07 -0300

    Saving and Fetching Tasks

--- Taskly/System/AppDelegate.swift
+++ Taskly/System/AppDelegate.swift
@@ -20,19 +20,13 @@ extension UINavigationController {
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    // Initialize TastStore
+    let taskStore = TaskStore()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        // Initialize TastStore
-        let taskStore = TaskStore()
-        
-//        let todoTask = [Task(name: "Meditate"), Task(name: "Buy Bananas"), Task(name: "Watch AAF"), Task(name: "Work"), Task(name: "Read a Book")]
-//        
-//        let doneTasks = [Task(name: "Watch Netflix"), Task(name: "Go to School")]
-//        
-//        taskStore.tasks = [todoTask, doneTasks]
         
         // Grab the TaskController
         let taskController = window?.rootViewController?.children.first
@@ -52,6 +46,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        // Save
+        TasksUtility.save(self.taskStore.tasks)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -64,6 +61,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+        // Save
+        TasksUtility.save(self.taskStore.tasks)
     }
 
 
